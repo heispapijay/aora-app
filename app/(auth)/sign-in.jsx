680314ed7d57c9d32eb3ba0139ba2e4505d9e849
heispapijay <heispapijay@gmail.com>
2024-04-23 21:12:30 +0300

    Refactor sign-in and sign-up screens, update dependencies

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -5,17 +5,17 @@ import { images } from '../../constants';
 import FormField from '../../components/FormField';
 import CustomButton from '../../components/CustomButton';
 import { Link, router } from 'expo-router';
-import { signIn } from '../../lib/appwrite';
+import { getCurrentUser, signIn } from '../../lib/appwrite';
 import { useGlobalContext } from '../../context/GlobalProvider';
 
 
 const SignIn = () => {
+  const { setUser, setIsLoggedIn } = useGlobalContext();
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const [form, setForm] = useState({
     email: '',
     password: '',
   });
-  const { setUser, setIsLoggedIn } = useGlobalContext();
-  const [isSubmitting, setIsSubmitting] = useState(false);
   const submit = async () => {
     if(!form.email === "" || !form.password === "") {
       Alert.alert('Error', 'Please fill all fields are required');
--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -9,8 +9,10 @@ import { getAllPosts, getLatestPosts } from '../../lib/appwrite'
 import useAppwrite from '../../lib/useAppwrite'
 import VidoeCard from '../../components/VidoeCard'
 import { StatusBar } from 'expo-status-bar'
+import { useGlobalContext } from '../../context/GlobalProvider'
 
 const Home = () => {
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
   const { data: posts, refetch } = useAppwrite(getAllPosts);
   const { data: latestPosts } = useAppwrite(getLatestPosts);
   const [refreshing, setRefreshing] = useState(false);
@@ -33,10 +35,10 @@ const Home = () => {
             <View className="justify-between items-start flex-row mb-6">
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">
-                  Welcome Back
+                  Welcome Back,
                 </Text>
                 <Text className="text-2xl font-psemibold text-white">
-                  John Doe
+                  {user?.username ?? "User"}
                 </Text>
               </View>
               <View className="mt-1.5">
